cfc84f696ca76d4a94fa497bb759b80ab3b3dfd9
Charles Pernet <charlespernet@gmail.com>
2019-03-13 18:19:35 +0100

    map done

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  }
+
   render() {
     return(
-      <div className="map-container"></div>
+      <div className="map-container">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDFuVYi8VmADnN_S7jcUmsWk3rYZB66mFI' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom} />
+      </div>
     )
   }
 }
+
+// <GoogleMapReact
+//   bootstrapURLKeys={{ key: 'AIzaSyDFuVYi8VmADnN_S7jcUmsWk3rYZB66mFI' }}
+//   defaultCenter={this.props.center}
+//   defaultZoom={this.props.zoom}
+// />
+  // TODO: MARKER
+  // <AnyReactComponent
+  //   lat={59.955413}
+  //   lng={30.337844}
+  //   text="My Marker"
+  // />
+// </GoogleMapReact>
 
 export default Map;
